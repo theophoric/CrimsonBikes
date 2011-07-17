6e0c0fc755f07cfa469efbc20ec533360b314807
Theodore Wilson <ted.a.wilson@gmail.com>
2011-07-17 16:33:55 -0400

    testing...

--- lib/checkout/cb_checkout.rb
+++ lib/checkout/cb_checkout.rb
@@ -35,10 +35,10 @@ module CbCheckout
         membership_options = Membership::TRIAL
       end
       puts "membership_options=#{membership_options}"
-      frontend = init_frontend
-      puts frontend
-      frontend.tax_table_factory = TaxTableFactory.new
-      checkout_command = frontend.create_checkout_command
+      @frontend = init_frontend
+      puts @frontend
+      @frontend.tax_table_factory = TaxTableFactory.new
+      checkout_command = @frontend.create_checkout_command
       cart = checkout_command.shopping_cart
       cart.private_data= {:user_id => user_id}      
       cart.create_item do |item|
