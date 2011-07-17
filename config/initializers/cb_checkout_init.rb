2d21b96f64a2bd5af89ab5c3b64ee8a289c67607
Theodore Wilson <ted.a.wilson@gmail.com>
2011-07-17 16:37:50 -0400

    testing...

--- config/initializers/cb_checkout_init.rb
+++ config/initializers/cb_checkout_init.rb
@@ -3,5 +3,5 @@ require 'cb_checkout'
 CbCheckout.configure do |config|
   config.merchant_id      = "618974739863729"
   config.merchant_key     = "4zkYk_TKGOxSsalUuKTZAw"
-  config.use_sandbox      = true
+  config.use_sandbox      = false
 end
