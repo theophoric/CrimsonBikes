084916b63a1a4f8a7bc9d94fb8b0671cb0d014f0
Theodore Wilson <ted.a.wilson@gmail.com>
2011-07-17 18:01:38 -0400

    testing

--- lib/checkout/membership.rb
+++ lib/checkout/membership.rb
@@ -1,7 +1,7 @@
 require 'google4r/checkout'
 module CbCheckout
   module Membership    
-    BASIC       = {:name => "Basic Crimson Bikes Membership", :description => "Enables you to checkout up to 1 bike at a time for a duration of up to 5 hours", :unit_price => Money.new(10, "USD"), :quantity => 1}
+    BASIC       = {:name => "Basic Crimson Bikes Membership", :description => "Enables you to checkout up to 1 bike at a time for a duration of up to 5 hours", :unit_price => Money.new(0, "USD"), :quantity => 1}
     PREMIUM     = {:name => "Premium Crimson Bikes Membership", :description => "Enables you to checkout up to 1 bike at a time for a duration of up to 5 hours", :unit_price => Money.new(0, "USD"), :quantity => 1}
     TRIAL       = {:name => "Trial Crimson Bikes Membership", :description => "Enables you to checkout up to 1 bike at a time for a duration of up to 5 hours", :unit_price => Money.new(0, "USD"), :quantity => 1}
     
