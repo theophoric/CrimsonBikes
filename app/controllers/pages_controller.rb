a192a2896e3d9a1d72e1de534a64403ffc1e77b7
Theodore Wilson <ted.a.wilson@gmail.com>
2011-09-02 12:42:57 -0400

    fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_filter {if user_signed_in? then  redirect_to (current_user.processed ? object_index_path("bikes") : account_path) end}
+  before_filter {if user_signed_in? then  redirect_to (current_user.active? ? object_index_path("bikes") : account_path) end}
   def welcome
     
   end
